0e3acf040e0ebb2c1caed7322f3bc1396615c2bc
trumpocalyspe <mark.jn.kim@gmail.com>
2018-02-22 16:07:46 -0800

    Models created

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+// connect Express app with MongoDB @ mLab using mongoose
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://markk:wdkym8ysNESW@ds245548.mlab.com:45548/local-library';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
