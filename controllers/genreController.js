e10d1d67c24ef0e69843c4c7f79feb098cc42441
trumpocalyspe <mark.jn.kim@gmail.com>
2018-02-28 17:03:00 -0800

    Book delete feature dependcy on book instances

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -119,7 +119,7 @@ exports.genre_delete_get = function(req, res, next) {
     }, function(err, results) {
         if (err) { return next(err); }
         if (results.genre==null) { // No results.
-          res.redirect('/catalog/genre');
+          res.redirect('/catalog/genres');
         }
         // Successful, so render.
         res.render('genre_delete', { title: 'Delete Genre', genre: results.genre, genre_books: results.genre_books } );
